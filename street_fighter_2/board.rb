d8c9c124d0ef0ce260e053b52f0e2a0f7c29ca8e
Miguel Angel Urbina <miguel.urbina15.1997@gmail.com>
2021-03-28 16:05:59 -0600

    Raname columns instead of lines

--- street_fighter_2/board.rb
+++ street_fighter_2/board.rb
@@ -4,7 +4,7 @@ end
 
 class StreetFighterBoard
   def initialize(characters, position = [0, 0])
-    @line_numer, @row_numer = position
+    @column_number, @row_number = position
     @characters = characters
   end
 
@@ -18,30 +18,30 @@ class StreetFighterBoard
   private
 
   def current_character
-    @characters[@line_numer][@row_numer]
+    @characters[@column_number][@row_number]
   end
 
   def up
-    @line_numer -= 1 unless @line_numer == 0
+    @column_number = 0
   end
 
   def down
-    @line_numer += 1 unless @line_numer == 1
+    @column_number = 1
   end
 
   def left
-    if @row_numer == 0
-      @row_numer = 5
+    if @row_number == 0
+      @row_number = 5
     else
-      @row_numer -= 1
+      @row_number -= 1
     end
   end
 
   def right
-    if @row_numer == 5
-      @row_numer = 0
+    if @row_number == 5
+      @row_number = 0
     else
-      @row_numer += 1
+      @row_number += 1
     end
   end
 
