7ea596b72d2dfc578718187c3eb040be2b36cbeb
Miguel Angel Urbina <miguel.urbina15.1997@gmail.com>
2021-03-28 15:50:36 -0600

    Create street fighter board

---
+++ street_fighter_2/board.rb
@@ -0,0 +1,47 @@
+def streetFighterSelection(fighters, position, moves)
+  StreetFighterBoard.new(fighters, position).move(moves)
+end
+
+class StreetFighterBoard
+  def initialize(characters, position = [0, 0])
+    @line_numer, @row_numer = position
+    @characters = characters
+  end
+
+  def move(moves)
+    moves.map do |move|
+      send(move)
+      current_character
+    end
+  end
+
+  private
+
+  def current_character
+    @characters[@line_numer][@row_numer]
+  end
+
+  def up
+    @line_numer -= 1 unless @line_numer == 0
+  end
+
+  def down
+    @line_numer += 1 unless @line_numer == 1
+  end
+
+  def left
+    if @row_numer == 0
+      @row_numer = 5
+    else
+      @row_numer -= 1
+    end
+  end
+
+  def right
+    if @row_numer == 5
+      @row_numer = 0
+    else
+      @row_numer += 1
+    end
+  end
+end
